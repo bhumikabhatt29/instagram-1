1b9d1335c5b963e51bc9bc8454c5b7e1c7bdea3c
Bhumika <bhumikabhatt29@gmail.com>
2023-05-18 21:05:15 +0530

    changes committed

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-let ejs = require('ejs');
+const ejs=require('ejs');
 const bodyParser=require('body-parser');
 const express=require('express');
-app.use(bodyParser.urlencoded({ extended: true }));
 let app=express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine',ejs) ; 
 app.use(express.static("public"));
 app.get("/",function(req,res){
     res.render("signup.ejs");
 });
-app.listen(3000,function(){
-    console.log("server is running on port 3000");
+app.get("/navbar",function(req,res){
+    res.render("navbar.ejs");
+});
+app.post('/',(req,res)=>{
+    res.render("navbar.ejs");
+});
+app.listen(3001,function(){
+    console.log("server is running on port 3001");
 })
+

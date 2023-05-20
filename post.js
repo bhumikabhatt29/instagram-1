7abf636f06fda186e6a5bb82200fc9a309238e0b
Divyansh Tanwar <divyanshtanwar04@gmail.com>
2023-05-20 10:46:01 +0530

    post section updated

--- post.js
+++ post.js
@@ -1,8 +1,14 @@
-const likeBtn = document.querySelector('.like-btn');
-const likeCount = document.querySelector('.like-count');
-let count = 0;
-
-likeBtn.addEventListener('click', () => {
-  count++;
-  likeCount.textContent = count;
-});
+var likes=1;
+function likeButton(){
+    console.log("entered the function");
+    let heart=document.querySelector('.heart');
+    let likes=document.querySelector('.likes');
+    if(heart.scr.match("heart.png")){
+        heart.src="heart_red.png";
+        likes.innerHTML=likes+=likes;
+    }
+    else
+    {
+        heart.src="heart.png";
+    }
+} 
